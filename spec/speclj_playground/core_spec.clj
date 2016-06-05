aa2d6e23cc2d2d259fb2c6216a6689a43f10f6bb
Burin Choomnuan <agilecreativity@gmail.com>
2016-06-04 22:44:59 -0400

    Add basic examples from InfoQ article

--- spec/speclj_playground/core_spec.clj
+++ spec/speclj_playground/core_spec.clj
@@ -13,4 +13,21 @@
   (it "is not false"
       (should-not false)))
 
+;; from InfoQ
+(describe "test my-reduce function"
+   (it "returns the intial value for empty list"
+       (should= 1 (my-reduce + 1 '())))
+
+   (it "adds initial value with one element in the list"
+       (should= 2 (my-reduce + 1 '(1))))
+
+   (it "returns correct value when more than one element in the list"
+       (should= 4 (my-reduce + 1 '(1 2))))
+
+   (it "returns 0 if input is empty"
+       (should= 0 (my-reduce + [])))
+
+   (it "returns 0 for multiplication on empty list"
+       (should= 1 (my-reduce * []))))
+
 (run-specs)
--- src/speclj_playground/core.clj
+++ src/speclj_playground/core.clj
@@ -1,1 +1,12 @@
 (ns speclj-playground.core)
+
+;; Simulate the build-in reduce function
+(defn my-reduce
+  ([f coll]
+   (if (empty? coll)
+     (f)
+     (my-reduce f (first coll) (rest coll))))
+  ([f val coll]
+   (if (empty? coll)
+     val
+     (my-reduce f (f val (first coll)) (rest coll)))))
